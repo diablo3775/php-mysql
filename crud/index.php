6034ab7aa33e1fb83bd3d8f03de596cca6ebcdc6
Anil Kumar <72590342+diablo3775@users.noreply.github.com>
2023-06-14 11:22:01 +0530

    Add files via upload

---
+++ crud/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+  <div class="container">
+    <h2>List of clients</h2>
+    <a class="btn btn-primary" href="/projects/php&mysql/create.php" role="button">New Client</a>
+    <br>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Address</th>
+          <th>Created At</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "myshop";
+        $connection = new mysqli($servername, $username, $password, $database);
+        if ($connection->connect_error) {
+          die("Connection failed: " . $connection->connect_error);
+        }
+
+        $sql = "SELECT * FROM clients";
+        $result = $connection->query($sql);
+
+        if (!$result) {
+          die("Invalid query:" . $connection->error);
+        }
+
+        // read data of each row
+        while ($row = $result->fetch_assoc()) {
+          echo "
+          <tr>
+          <td>$row[id]</td>
+          <td>$row[name]</td>
+          <td>$row[email]</td>
+          <td>$row[phone]</td>
+          <td>$row[address]</td>
+          <td>$row[created_at]</td>
+          <td>
+            <a class='btn btn-primary btn-sm' href='/projects/php&mysql/edit.php?id=$row[id]'>Edit</a>
+            <a class='btn btn-danger btn-sm' href='/projects/php&mysql/delete.php?id=$row[id]'>Delete</a>
+          </td>
+        </tr>
+          ";
+        }
+        ?>
+      </tbody>
+    </table>
+  </div>
+</body>
+</html>
